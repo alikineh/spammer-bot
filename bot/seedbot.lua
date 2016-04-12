aa75fcb707c1ce23b4aba9a6dd8b8e8abf76191f
alikineh <000xxx000information000xxx000@gmail.com>
2016-04-12 20:32:45 +0430

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -226,29 +226,10 @@ function create_config( )
     "id",
     "admin"
     },
-    sudo_users = {85831686},--Sudo users
+    sudo_users = {153862670},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
-    about_text = [[spammer-bot
-An advance Administration bot based on teleseed 
-spammer bot open sourced to:
-[source spammer bot](https://github.com/3pehrdev/spammer-bot)
-
-developed and founded
-By
-[blacklife admin](telegram.me/joker_admin_1)
-my chanell
-[chanell blacklifeTM](telegram.me/blacklifech)
-
- thanks to:
-mobin
---
---
---
-
-special thanks to
-blacklifeTm
-teleseedTm
+    about_text = [[😐
 ]],
     help_text_realm = [[
 Realm Commands:
