cf60961b6111bea4dca10f257bbfee24fe882af2
alikineh <000xxx000information000xxx000@gmail.com>
2016-04-18 13:55:31 +0430

    Update spam_1a.lua

--- plugins/spam_1a.lua
+++ plugins/spam_1a.lua
@@ -1,36 +1,21 @@
-local add_user_cfg = load_from_file('data/add_user_cfg.lua')
 function chat_new_user_link(msg)
    local pattern = add_user_cfg.initial_chat_msg
+
+if matches[1] == "chat_add_user"  and is_sudo(msg) then
+	return [[ spam
+	]]
+ end
 end
-
-function chat_new_user(msg)
-   local pattern = add_user_cfg.initial_chat_msg
-      end
-      local sambutan = "spam"
-
-local function run(msg, matches)
-   if not msg.service then
-      return "Are you trying to troll me?"
-   end
-   --vardump(msg)
-   if matches[1] == "chat_add_user" then
-  end   
-  
-      chat_new_user_link(msg)
-      description_rules(msg, nama)
-   elseif matches[1] == "chat_del_user" then
-       local bye_name = msg.action.user.first_name
-       return 'spam'
-   end
-end
-
-return {
-   description = "Service plugin that sends a custom message when an user enters a chat.",
-   usage = "Welcoming new member.",
-   patterns = {
-      "^!!tgservice (chat_add_user)$",
-      "^!!tgservice (chat_add_user_link)$",
-      "^!!tgservice (chat_del_user)$",
-   },
-   run = run
+ return {
+	description = "Chat With Robot Server",
+	usage = {
+	    sudo = {
+	    "chat_add_user : send chat_add_user to chat" },
+	},
+	patterns = {
+		"^chat_add_user$",
+},
+	run = run,
+    --privileged = true,
+	pre_process = pre_process
 }
