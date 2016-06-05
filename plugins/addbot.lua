de117339734f7a5160ab72c1ac9ce3ca84b70cdb
alikineh <000xxx000information000xxx000@gmail.com>
2016-06-05 21:06:55 +0430

    Update addbot.lua

--- plugins/addbot.lua
+++ plugins/addbot.lua
@@ -1,54 +1,48 @@
---[[
-Bot can join into a group by replying a message contain an invite link or by
-typing !add [invite link].
-URL.parse cannot parsing complicated message. So, this plugin only works for
-single [invite link] in a post.
-[invite link] may be preceeded but must not followed by another characters.
---]]
+-- Invite other user to the chat group.
+-- Use !invite name User_name or !invite id id_number
+-- The User_name is the print_name (there are no spaces but _)
 
 do
 
-  local function parsed_url(link)
-    local parsed_link = URL.parse(link)
-    local parsed_path = URL.parse_path(parsed_link.path)
-    i = 0
-    for k,segment in pairs(parsed_path) do
-      i = i + 1
-      if segment == 'joinchat' then
-        invite_link = string.gsub(parsed_path[i+1], '[ %c].+$', '')
-        break
-      end
-    end
-    return invite_link
+local function callback(extra, success, result)
+  vardump(success)
+  vardump(result)
+end
+
+local function run(msg, matches)
+  local user = matches[2]
+
+  -- User submitted a user name
+  if matches[1] == "name" then
+    user = string.gsub(user," ","_")
+  end
+  
+  -- User submitted an id
+  if matches[1] == "id" then
+    user = 'user#id'..user
   end
 
-  local function action_by_reply(extra, success, result)
-    local hash = parsed_url(result.text)
-    join = import_chat_link(hash, ok_cb, false)
+  -- The message must come from a chat group
+  if msg.to.type == 'chat' then
+    local chat = 'chat#id'..msg.to.id
+    chat_add_user(chat, user, callback, false)
+    return "Add: "..user.." to "..chat
+  else 
+    return 'This isnt a chat group!'
   end
 
-  function run(msg, matches)
-    if is_sudo(msg) then
-      if msg.reply_id then
-        msgr = get_message(msg.reply_id, action_by_reply, {msg=msg})
-      elseif matches[1] then
-        local hash = parsed_url(matches[1])
-        join = import_chat_link(hash, ok_cb, false)
-      end
-    end
-  end
+end
 
-  return {
-    description = 'Invite the bot into a group chat via its invite link.',
-    usage = {
-      '!AddBot : Join a group by replying a message containing invite link.',
-      '!AddBot [invite_link] : Join into a group by providing their [invite_link].'
-      },
-    patterns = {
-      '^[/!](addBot)$',
-      '^[/!](ddBot) (.*)$'
-    },
-    run = run
-  }
+return {
+  description = "Invite other user to the chat group", 
+  usage = {
+    "!invite name [user_name]", 
+    "!invite id [user_id]" },
+  patterns = {
+    "^!invite (name) (.*)$",
+    "^!invite (id) (%d+)$"
+  }, 
+  run = run 
+}
 
 end
