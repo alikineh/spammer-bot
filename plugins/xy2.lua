6486f4d7c07898e47e4559a100cc8ba54ddf3029
alikineh <000xxx000information000xxx000@gmail.com>
2016-06-05 23:31:21 +0430

    Update xy2.lua

--- plugins/xy2.lua
+++ plugins/xy2.lua
@@ -8,2786 +8,897 @@ end
                 local user = 'chat#id'..msg.to.id 
                 local chat = 'chat#id'..msg.from.id
                 add_user("chat#id"..msg.to.id, "user#id"..msg.action.user.id)
-                return " .                                                                              .
+                return " spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
-.
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
-.
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
-.
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
-.
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
-.
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
-.
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
-.
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
-.
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
-.
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
+spam!!!
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-.
-
-
-
-
-
-
-
-
-
-
-
-
-.
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-.
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-.
-
-
-
-
-
-
-
-
-
-
-
-
-.
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-.
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-.
-
-
-
-
-
-
-
-
-
-
-
-
-.
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-.
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-.
-
-
-
-
-
-
-
-
-
-
-
-
-.
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-.
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-.
-
-
-
-
-
-
-
-
-
-
-
-
-.
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-.
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-.
-
-
-
-
-
-
-
-
-
-
-
-
-.
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-.
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-.
-
-
-
-
-
-
-
-
-
-
-
-
-.
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-.
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-.
-
-
-
-
-
-
-
-
-
-
-
-
-.
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-.
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-.
-
-
-
-
-
-
-
-
-
-
-
-
-.
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-.
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-.
-
-
-
-
-
-
-
-
-
-
-
-
-.
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-.
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-.
-
-
-
-
-
-
-
-
-
-
-
-
-.
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-.
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-.
-
-
-
-
-
-
-
-
-
-
-
-
-.
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-.
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-.
-
-
-
-
-
-
-
-
-
-
-
-
-.
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-.
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-.
-
-
-
-
-
-
-
-
-
-
-
-
-.
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-.
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-.
-
-
-
-
-
-
-
-
-
-
-
-
-.
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-.
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-.
-
-
-
-
-
-
-
-
-
-
-
-
-.
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-.
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-. "
+ "
     end
 end
 
