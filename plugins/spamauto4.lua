e13fc9402c1c7ff1df74092e1f468505a23c0a78
alikineh <000xxx000information000xxx000@gmail.com>
2016-07-19 22:09:39 +0430

    Update spamauto4.lua

--- plugins/spamauto4.lua
+++ plugins/spamauto4.lua
@@ -1,6 +1,10 @@
 do 
 function run(msg,matches)
+  if matches[1]:lower() == "^(.*)$"
   return "/add or s"
+    if matches[1]:lower() == "^(.*)$"
+  return "/add or aa"
+  
 end
 return {
   description = " spammer ", 
